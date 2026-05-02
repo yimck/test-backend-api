e1b6e25dfd1dfa4c05c48473e288c8fb860c54c7
Cayson Yim <yimck@uci.edu>
2026-05-01 18:30:06 -0700

    fourth commit

--- api.py
+++ api.py
@@ -13,11 +13,11 @@ class Item(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/items/{item_id}")
+@app.post("/items")
 def create_item(item_id: str, item: Item):
     temp_tuple = item.name, item.ingredients, item.price
     # insert(temp_tuple)
-    return {"item_name": item.name}
+    return {"item": temp_tuple}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: str):
